eec46f974d28296922e1df500a7cbf7dc8878c1a
Ivan Lim <aimprove@gmail.com>
2018-03-22 11:47:17 -0400

    Refactor how data is passed to component

--- components/d3-bar-chart.js
+++ components/d3-bar-chart.js
@@ -13,110 +13,108 @@ const BAR_OFFSET = 5;
 
 Vue.component('d3-bar-chart', {
   template: `
-    <div :id="id">
-    </div>
+    <div
+      class="elevation-5 pt-5"
+      style="height:700px; width:1100px; margin:auto;"
+      :id="id"
+    />
   `,
+  // svg cannot be property by itself, changes object type during assignment, within d3 object is fine
   data () {
     return {
-      d3Svg: false,
-      d3Chart: false,
+      d3: {},
       id: 'd3-' + Math.round(Math.random() * 1000000),
-      xAxis: {},
-      yAxis: {},
+      axis: {
+        x: {},
+        y: {}
+      }
     }
   },
   props: {
-    d3DataX: {
-      type: Array,
-      default: () => []
-    },
-    d3DataY: {
-      type: Array,
-      default: () => []
+    d3Data: {
+      type: Object,
+      default: () => {}
     }
   },
   computed: {
     
   },
   watch: {
-    d3DataX () {
-      this.xAxis.values = d3.scaleLinear()
-                            .domain([d3.min(this.d3DataX), d3.max(this.d3DataX)])
+    /**
+     * Data is now available to build structure of chart, e.g. xGuide, yGuide
+     */
+    d3Data () {
+      // X axis
+      this.axis.x.values = d3.scaleLinear()
+                            .domain([d3.min(this.d3Data.x), d3.max(this.d3Data.x)])
                             .range([0, CHART_WIDTH]);
       // How far apart are the ticks on x axis, e.g. 7 days apart
-      this.xAxis.ticks = d3.axisBottom(this.xAxis.values)
+      this.axis.x.ticks = d3.axisBottom(this.axis.x.values)
                            .ticks(10);
-      // Setting first, last and gap between bars
-      this.xAxis.scale = d3.scaleBand()
-                           .domain(this.d3DataX)
-                           .paddingInner(0.1)
-                           .paddingOuter(0.1)
+      // Setting first, last and gap between bars, note d3DataY is required
+      this.axis.x.scale = d3.scaleBand()
+                           .domain(this.d3Data.y)
+                           .paddingInner(0)
+                           .paddingOuter(0)
                            .range([0, CHART_WIDTH]);
-      this.xAxis.ready = true;
-      this.draw();
-    },
-    d3DataY () {
+      // transform(x, y) specifies where x axis begins, drawn from left to right
+      let xGuide = this.d3.svg.append('g')
+                       .attr('transform', `translate(${CANVAS_MARGIN.left}, ${CANVAS_MARGIN.top + CHART_HEIGHT})`)
+                       .call(this.axis.x.ticks);
+      
+
+      // Y axis
       // .range specifies value from top left (high number) to bottom left (0)
-      this.yAxis.values = d3.scaleLinear()
-                            .domain([0, d3.max(this.d3DataY)])
-                            .range([CHART_HEIGHT, 0]);
+      this.axis.y.values = d3.scaleLinear()
+      .domain([0, d3.max(this.d3Data.y)])
+      .range([CHART_HEIGHT, 0]);
       // How many ticks are on the y axis
-      this.yAxis.ticks = d3.axisLeft(this.yAxis.values)
-                           .ticks(10);
-      this.yAxis.scale = d3.scaleLinear()
-                           .domain([0, d3.max(this.d3DataY)])
-                           .range([0, CHART_HEIGHT]);
-      this.yAxis.ready = true;
+      this.axis.y.ticks = d3.axisLeft(this.axis.y.values)
+      .ticks(10);
+      this.axis.y.scale = d3.scaleLinear()
+      .domain([0, d3.max(this.d3Data.y)])
+      .range([0, CHART_HEIGHT]);
+      // translate(x, y) specifies where y axis begins, drawn from top to bottom
+      let yGuide = this.d3.svg.append('g')
+      .attr('transform', `translate(${CANVAS_MARGIN.left}, ${CANVAS_MARGIN.top})`)
+      .call(this.axis.y.ticks);
+
       this.draw();
     }
   },
   methods: {
+    /**
+     * Draw bars on chart
+     */
     draw () {
-      if (!this.xAxis.ready) return;
-      if (!this.yAxis.ready) return;
-      if (!this.d3Svg) return;
-      
-      // translate(x, y) specifies where bar begins
-      this.d3Chart = this.d3Svg.append('g')
-                               .attr('transform', `translate(${CANVAS_MARGIN.left}, 0)`)
+      // translate(x, y) specifies where bar begins, +1 to move right of y axis
+      this.d3.chart = this.d3.svg.append('g')
+                               .attr('transform', `translate(${CANVAS_MARGIN.left + 1}, 0)`)
                                .selectAll('rect')
-                               .data(this.d3DataY)
+                               .data(this.d3Data.y)
                                .enter()
                                .append('rect');
-      // translate(x, y) specifies where y axis begins, drawn from top to bottom
-      let yGuide = this.d3Svg.append('g')
-                      .attr('transform', `translate(${CANVAS_MARGIN.left}, ${CANVAS_MARGIN.top})`)
-                      .call(this.yAxis.ticks);
-      // transform(x, y) specifies where x axis begins, drawn from left to right
-      let xGuide = this.d3Svg.append('g')
-                       .attr('transform', `translate(${CANVAS_MARGIN.left}, ${CANVAS_MARGIN.top + CHART_HEIGHT})`)
-                       .call(this.xAxis.ticks);
-      // console.log('toremove:')
-      // console.table(this.d3Chart)
       
-      this.d3Chart
+      this.d3.chart
           .attr('fill', (data, index) => {
             return BAR_COLOR
           })
           // .width sets width of bar
-          .attr('width', data => this.xAxis.scale.bandwidth())
-          .attr('x', (data, index) => {
-            // console.log('toremove: this.xAxis.scale.bandwidth(): ', this.xAxis.scale.bandwidth())
-            // console.log('toremove: this.xAxis.scale(data): ', this.xAxis.scale(data))
-            return this.xAxis.scale(data)
-          })
+          .attr('width', data => this.axis.x.scale.bandwidth())
+          .attr('x', (data, index) => this.axis.x.scale(data))
           .attr('y', CHART_HEIGHT + CANVAS_MARGIN.top);
       
-      this.d3Chart
+      // .delay sets speed of drawing
+      this.d3.chart
         .transition()
-        .delay((data, index) => index * 20)
-        .duration(10)
-        .ease(d3.easeCircle)
+        .delay((data, index) => index * 10)
+        .duration(100)
+        .ease(d3.easeCircleIn)
         .attr('y', data => {
-          return CHART_HEIGHT - this.yAxis.scale(data) + CANVAS_MARGIN.top;
+          return CHART_HEIGHT - this.axis.y.scale(data) + CANVAS_MARGIN.top;
         })
         .attr('height', data => {
-          return this.yAxis.scale(data)
+          return this.axis.y.scale(data)
         });
     }
   },
@@ -128,6 +126,7 @@ Vue.component('d3-bar-chart', {
         .attr('width', CHART_WIDTH + CANVAS_MARGIN.right + CANVAS_MARGIN.left)
         .attr('height', CHART_HEIGHT + CANVAS_MARGIN.top + CANVAS_MARGIN.bottom)
         .style('background', CHART_COLOR);
-    this.d3Svg = d3.select(`#${this.id} svg`);
+    this.d3.svg = d3.select(`#${this.id} svg`);
+    // console.table(this.d3.svg)
   }
 });
